4216c3fb9dae47bb6f7e713974d46c90e769d87b
TonyPythoneer <tonypythoneer@gmail.com>
2015-12-16 16:11:43 +0800

    Enhance: Add docstring in RestfulStatusMiddleware in middleware/status.py

--- perfect-drf-project/perfect-drf-project/api_root/middleware/status.py
+++ perfect-drf-project/perfect-drf-project/api_root/middleware/status.py
@@ -14,13 +14,15 @@ from rest_framework.response import Response
 
 class RestfulStatusMiddleware(object):
     """RestfulStatusMiddleware
+
+    According to `The Response class extends SimpleTemplateResponse` in
+    Response of DRF document, when it sets data in data of response, it
+    will trigger to render.
+
+    However, data of attributes of response is `The unrendered content
+    of a Request object.`, if it needs to be modified, it has to make
+    in process_template_response.
     """
-    def is_rest_framework_response(self, response):
-        return isinstance(response, Response)
-
-    def is_dict_of_data(self, data):
-        return isinstance(data, dict)
-
     def process_template_response(self, request, response):
         """process_template_response
         """
@@ -34,3 +36,9 @@ class RestfulStatusMiddleware(object):
                 if status.is_client_error(response.status_code):
                     response.data['status'] = 'error'
         return response
+
+    def is_rest_framework_response(self, response):
+        return isinstance(response, Response)
+
+    def is_dict_of_data(self, data):
+        return isinstance(data, dict)
